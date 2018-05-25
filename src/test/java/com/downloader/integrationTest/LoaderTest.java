c0e45ca3046e016f6f1c8ebc4f6cb5010b2b4b72
Khwanchanok Srimool <khwanchanok.sri@ascendcorp.com>
2018-05-26 04:53:22 +0700

    remove local path and unnecessary imports

--- src/test/java/com/downloader/integrationTest/LoaderTest.java
+++ src/test/java/com/downloader/integrationTest/LoaderTest.java
@@ -1,7 +1,5 @@
 package com.downloader.integrationTest;
 
-import com.downloader.loader.FtpLoader;
-import com.downloader.loader.HttpLoader;
 import com.downloader.loader.Loader;
 import org.junit.Test;
 
@@ -14,7 +12,7 @@ public class LoaderTest {
     public void downloadHttpFileSuccess() throws Exception{
         Loader httpLoader = new Loader();
         String targetUrl = "https://download.applied-maths.com/sites/default/files/download/Antibiotics%20sample%20data.zip";
-        String savePath = "/Users/khwanchanok/Downloads/output.zip";
+        String savePath = "/output.zip";
         httpLoader.download(targetUrl, savePath);
     }
 
@@ -22,7 +20,7 @@ public class LoaderTest {
     public void downloadFtpFileSuccess() throws Exception {
         Loader ftpLoader = new Loader();
         String targetUrl = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data/README_data_has_moved.md";
-        String savePath = "/Users/khwanchanok/Downloads/output.md";
+        String savePath = "/output.md";
         ftpLoader.download(targetUrl, savePath);
     }
 
